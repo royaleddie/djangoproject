414fbe298130f220420b1565fb9c34cc29b1ab23
royaleddie <iginuaendurance@gmail.com>
2023-02-10 00:23:43 +0100

    Update

--- blog/views.py
+++ blog/views.py
@@ -1,28 +1,13 @@
 from django.shortcuts import render
+from .models import Post
 
-
-posts = [
-    {
-        'title' : 'Royal',
-        'author' : 'Roy Ray',
-        'datPosted' : '23re june, 2009',
-        'content' : 'jfjsldjklk'
-    },
-
-    {
-        'title' : 'Royal',
-        'author' : 'Roy Ray',
-        'datPosted' : '23re june, 2009',
-        'content' : 'jfjsldjklk'
-    }
-]
 
 def home(request):
     context = {
-        'posts' : posts
+        'posts': Post.objects.all()
     }
     return render(request, 'blog/home.html', context)
 
 
 def about(request):
-    return render(request, 'blog/about.html', {'title' : 'About'})
+    return render(request, 'blog/about.html', {'title': 'About'})
